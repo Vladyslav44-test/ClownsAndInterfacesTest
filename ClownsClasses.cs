282b641b4ceda1742ff466ae0046aef1de086e3d
Vladislav44-test <elertvelert@gmail.com>
2026-09-18 19:13:46 +0300

    Додав класи FunnyFunny (реалізує інтерфейс IClown) та ScaryScary (успадковується від FunnyFunny та реалізує інтерфейс IScaryClown)

---
+++ ClownsClasses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClownsAndInterfacesTest
+{
+    internal class FunnyFunny : IClown
+    {
+        private string funnyThingIHave;
+
+        public string FunnyThingIHave { get { return funnyThingIHave; } }
+
+        public void Honk()
+        {
+            Console.WriteLine($"Hi kids! I have a {FunnyThingIHave}.");
+        }
+
+        public FunnyFunny(string funnyThing)
+        {
+            funnyThingIHave = funnyThing;
+        }
+    }
+
+    class ScaryScary : FunnyFunny, IScaryClown
+    {
+        private short scaryThingCount;
+        private string funnyThingIHave;
+
+        public string ScaryThingIHave { get { return scaryThingCount + " spiders"; } }
+        public string FunnyThingIHave { get { return funnyThingIHave; } }
+
+        public void ScareLittleChildren()
+        {
+            Console.WriteLine($"Boo! Gotcha! Look at my {ScaryThingIHave}!");
+        }
+
+        public ScaryScary(short scaryThingCount) : base("spiders")
+        {
+            this.scaryThingCount = scaryThingCount;
+        }
+    }
+}
