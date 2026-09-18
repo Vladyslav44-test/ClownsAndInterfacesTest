4c8ab651507a13e2626523db4bda797ac79cda1f
Vladislav44-test <elertvelert@gmail.com>
2026-09-18 18:58:05 +0300

    Додав інтерфейси IClown та IScaryClown, який реалізує перший інтерфейс

---
+++ ClownInterfaces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClownsAndInterfacesTest
+{
+    internal interface IClown
+    {
+        string FunnyThingIHave { get; }
+
+        void Honk();
+    }
+
+    interface IScaryClown : IClown
+    {
+        string ScaryThingIHave { get; }
+
+        void ScareLittleChildren();
+    }
+}
