2baf5e134111eaddb4f212943a791f8ce0c98e9e
Vladislav44-test <elertvelert@gmail.com>
2026-09-22 17:28:20 +0300

    Змінив інтерфейс IClown, додавши в нього статичні компоненти та реалізації за замовчуванням

--- ClownInterfaces.cs
+++ ClownInterfaces.cs
@@ -8,9 +8,25 @@ namespace ClownsAndInterfacesTest
 {
     internal interface IClown
     {
+        protected static Random random = new Random();
+        private static int carCapacity = 12;
+
         string FunnyThingIHave { get; }
+        public static int CarCapacity
+        {
+            get { return carCapacity; }
+            set
+            {
+                if (value > 10) carCapacity = value;
+                else Console.Error.WriteLine($"Warning: Car capacity {value} is too small");
+            }
+        }
 
         void Honk();
+        public static string ClownCarDescription()
+        {
+            return $"A clown car with {random.Next(CarCapacity / 2, CarCapacity)} clowns";
+        }
     }
 
     interface IScaryClown : IClown
