78646777523e26bbd3cfff80d94a248c30254286
Vladislav44-test <elertvelert@gmail.com>
2026-09-22 17:31:39 +0300

    Додав код в метод Main, який застосовує статичні компоненти інтерфейсу IClown

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace ClownsAndInterfacesTest
     {
         static void Main(string[] args)
         {
+            IClown.CarCapacity = 18;
+            Console.WriteLine(IClown.ClownCarDescription());
             IClown fingersTheClown = new ScaryScary("big red nose", 14);
             fingersTheClown.Honk();
             if (fingersTheClown is IScaryClown iScaryClownReference) iScaryClownReference.ScareLittleChildren();
